4d1707f8cf0339dcb8dd1d2fcf23b9e025b33da5
Igor <idutyshev@gmail.com>
2026-04-07 22:28:22 +0300

    Initial commit

---
+++ geoFields/src/main/java/com/geofields/GeoFieldsApplication.java
@@ -0,0 +1,13 @@
+package com.geofields;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoFieldsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoFieldsApplication.class, args);
+    }
+
+}
---
+++ geoFields/src/test/java/com/geofields/GeoFieldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geofields;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoFieldsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
